95b8232bffbb4f0c82a7207bc323cbf2d48b9952
buriusz <burek.piotr@gmail.com>
2023-08-16 10:40:08 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = 'f'; // lub teź bool np. true ze jest kobieta a false mezczyzna lub na odwrót, albo string "kobieta" lub "mężczyzna"
+var age = 33;
+
+if (sex == 'f' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != 'f' && age < 18)
+{
+    Console.WriteLine($"Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nie jest to kobieta w wieku poniżej 30, nie jest to Ewa w wieku 30 lat a tym bardziej mężczyzna w wieku poniżej 18 lat");
+}
