bab65725d588ffb75d6617877dba9e64369b3092
buriusz <burek.piotr@gmail.com>
2023-08-27 10:58:55 +0200

    Purified files

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,33 +1,8 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Adam", "Kamizelich");
-employee.AddGrade("Adam");
+employee.AddGrade(9f);
 employee.AddGrade("4000");
-employee.AddGrade(2123564);
-employee.AddGrade(30.0d);
-employee.AddGrade(50);
-var statistics = employee.GetStatistics();
-var statistics1 = employee.GetStatisticsWithForEach();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWhile();
+employee.AddGrade(2);
+employee.AddGrade(6);
 
-Console.WriteLine("Statistics with 'foreach loop'");
-Console.WriteLine($"Average: {statistics1.Average:N2}");
-Console.WriteLine($"Min: {statistics1.Min}");
-Console.WriteLine($"Max: {statistics1.Max}");
-
-Console.WriteLine("Statistics with 'for loop'");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
-
-Console.WriteLine("Statistics with 'do while loop'");
-Console.WriteLine($"Average: {statistics3.Average:N2}");
-Console.WriteLine($"Min: {statistics3.Min}");
-Console.WriteLine($"Max: {statistics3.Max}");
-
-Console.WriteLine("Statistics with 'while loop'");
-Console.WriteLine($"Average: {statistics4.Average:N2}");
-Console.WriteLine($"Min: {statistics4.Min}");
-Console.WriteLine($"Max: {statistics4.Max}");
